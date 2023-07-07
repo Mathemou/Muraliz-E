57507d867ccc2b8d216c48912db0ce142c9c836f
Matheus <matheussantosmoura2000@gmail.com>
2023-07-06 23:55:59 -0300

    Solicitações implementadas

---
+++ app/src/main/java/com/example/muralize/Views/SolicitacaoExpandida.kt
@@ -0,0 +1,11 @@
+package com.example.muralize.Views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SolicitacaoExpandida : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_solicitacao_expandida)
+    }
+}
